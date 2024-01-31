f39c8941491d0f6480a7042bf1ca7f8926bd628f
rhit-wighths <wighths@rose-hulman.edu>
2024-01-30 22:06:53 -0500

    outline for investments

---
+++ RetireSimple.NewEngine/New_Engine/Investments/Bond.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RetireSimple.NewEngine.New_Engine.Investments {
+	public class Bond : Investment {
+
+		public Bond(string symbol, string name, double price, double quantity)
+		: base(symbol, name, price, quantity) {
+
+		}
+
+
+	}
+}
---
+++ RetireSimple.NewEngine/New_Engine/Investments/Stock.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RetireSimple.NewEngine.New_Engine.Investments {
+	public class Stock : Investment {
+
+		public Stock(string symbol, string name, double price, double quantity)
+		: base(symbol, name, price, quantity) {
+
+		}
+	}
+}
