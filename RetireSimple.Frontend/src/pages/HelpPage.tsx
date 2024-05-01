330a2e691f24cd2506d97a558220119f828585c8
Brayden Davis <davisba@rose-hulman.edu>
2024-05-01 18:16:30 -0400

    fixed bug regarding help page

--- RetireSimple.Frontend/src/pages/HelpPage.tsx
+++ RetireSimple.Frontend/src/pages/HelpPage.tsx
@@ -12,14 +12,6 @@ export function HelpPage() {
 	const imageUrls = [
 		'/images/HomePage.PNG',
 	];
-
-	const goToPrevious = () => {
-		setCurrentIndex((prevIndex) => (prevIndex === 0 ? imageUrls.length - 1 : prevIndex - 1));
-	};
-    
-	const goToNext = () => {
-		setCurrentIndex((prevIndex) => (prevIndex + 1) % imageUrls.length);
-	};
   
 	return (
 		<div>
@@ -28,20 +20,10 @@ export function HelpPage() {
 			<SetUpNav />
 			<div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
 				<div style={{ display: 'flex', marginBottom: '10px' }}>
-					<button onClick={goToPrevious}>
-						<Icon sx={{ fontSize: 36 }}>arrow_backward</Icon>
-					</button>
 					<div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
 						<img style={{ maxWidth: '100%', minHeight: '100%' }} 
 							src={imageUrls[currentIndex]} alt={`Image ${currentIndex + 1}`} />
-						<div style={{ position:'relative', bottom:'-10px', maxWidth:'fit-content', color: 'white', fontSize: '18px', backgroundColor: 'rgba(0, 0, 0, 0.5)', padding: '5px 10px', borderRadius: '5px' }}>
-							Page {currentIndex + 1} of {imageUrls.length}
-						</div>
-
 					</div>
-					<button onClick={goToNext}>
-						<Icon sx={{ fontSize: 36 }}>arrow_forward</Icon>
-					</button>
 				</div>
 			</div>
 		</div>
